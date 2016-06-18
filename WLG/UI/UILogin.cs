a204ad9c56ab878a9d53cccf144ead89425a0bd2
MCKolumfer <mckolumfer@gmail.com>
2016-06-18 16:49:56 +0200

    Projektdateien hinzufügen.

---
+++ WLG/UI/UILogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WLG.UI
+{
+    public partial class UILogin : Form
+    {
+        public UILogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
